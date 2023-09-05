f22b29993377c04a1eb61615ce1c855446920e03
6331503001 <97874391+6331503001@users.noreply.github.com>
2023-09-05 21:46:25 +0700

    Update TestBMICalculatorIT.java

--- src/test/java/th/mfu/TestBMICalculatorIT.java
+++ src/test/java/th/mfu/TestBMICalculatorIT.java
@@ -1,53 +1,72 @@
 package th.mfu;
 
-import java.io.IOException;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
 
-//TODO: add webservlet to "/calbmi"
-@WebServlet(  "/calbmi")
-public class BMICalculatorServlet extends HttpServlet{
+import org.hamcrest.CoreMatchers;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
-    @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO: get parameter from request: "weight" and "height"
-        String weightStr= request.getParameter("weight");
-        String heightStr= request.getParameter("height");
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-        double height = Double.parseDouble(heightStr);
-        double weight = Double.parseDouble(weightStr);
+public class TestBMICalculatorIT {
+    private static Client client;
+    private static Logger _logger = LoggerFactory.getLogger(TestBMICalculatorIT.class);
+    private static String WEB_URI = "http://localhost:8080/calbmi";
 
-        
-        //TODO: calculate bmi
-        double bmi = weight/(height*height);
-        int BMI = (int) Math.round(bmi);
-
-        //TODO: determine the built from BMI
-        String result="";
-        if(BMI < 18.5){
-            result= "underweight";
-        }else if(18.5 <= BMI && BMI< 25  ){
-            result = "normal";
-        }else if(25 <= BMI && BMI< 30){
-            result = "overweight";
-        }else if(30 <= BMI && BMI< 35){
-            result = "obese";
-        }else if(BMI >= 35){
-            result= "extremely obese";
-
-        }
-        
-        //TODO: add bmi and built to the request's attribute
-        request.setAttribute("result",result);
-        request.setAttribute("BMI", BMI);
-        //TODO: forward to jsp
-        request.getRequestDispatcher("bmi_result.jsp").forward(request,response);
-
-           
+    @BeforeClass
+    public static void createClient() {
+        // Use ClientBuilder to create a new client that can be used to create
+        // connections to the Web service.
+        client = ClientBuilder.newClient();
     }
-    
+
+    @AfterClass
+    public static void closeConnection() {
+        client.close();
+    }
+
+    @Test
+    public void testCaclulate1() {
+
+        // Make a HTTP GET request to retrieve the last created Parolee.
+        try (Response response = client.target(WEB_URI+"?weight=70&height=1.5").request().get()) {
+
+            // Check that the HTTP response code is 200 OK.
+            int responseCode = response.getStatus();
+            assertEquals(200, responseCode);
+
+            String jsonResponse = response.readEntity(String.class);
+            assertThat(jsonResponse, CoreMatchers.containsString("Result is 31"));
+
+            assertThat(jsonResponse, CoreMatchers.containsString("obese"));
+            _logger.info("IT1 test passed");
+        }
+    }
+
+    //TODO: add another test case for normal built such as weight=50 and height=1.5.
+    @Test
+    public void testCaclulate2() {
+
+        // Make a HTTP GET request to retrieve the last created Parolee.
+        try (Response response = client.target(WEB_URI+"?weight=50&height=1.5").request().get()) {
+
+            // Check that the HTTP response code is 200 OK.
+            int responseCode = response.getStatus();
+            assertEquals(200, responseCode);
+
+            String jsonResponse = response.readEntity(String.class);
+            assertThat(jsonResponse, CoreMatchers.containsString("Result is 22"));
+
+            assertThat(jsonResponse, CoreMatchers.containsString("normal"));
+            _logger.info("IT1 test passed");
+        }
+    }
+
 }
